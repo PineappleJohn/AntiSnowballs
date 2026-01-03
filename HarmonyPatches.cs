6fdb39991560db745d78afb3a7409ec08c557b04
John <johnsillymonk@gmail.com>
2026-01-03 15:00:54 -0600

    Init

---
+++ HarmonyPatches.cs
@@ -0,0 +1,44 @@
+﻿using AntiSnowballs;
+using ExitGames.Client.Photon;
+using GorillaLocomotion;
+using HarmonyLib;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace AntiSnowballs
+{
+    public class HarmonyPatches
+    {
+        static Harmony instance;
+        static bool isPatched = false;
+        public static bool IsPatched
+        {
+            get => isPatched;
+            set
+            {
+                if (IsPatched == value || (instance == null && !value)) return;
+                isPatched = value;
+
+                if (value)
+                {
+                    instance ??= Harmony.CreateAndPatchAll(typeof(Plugin).Assembly, "John.PineappleP");
+                    return;
+                }
+
+                instance.UnpatchSelf();
+                instance = null;
+            }
+        }
+        public static void ApplyHarmonyPatches() => IsPatched = true;
+        public static void RemoveHarmonyPatches() => IsPatched = false;
+    }
+
+    [HarmonyPatch(typeof(GTPlayer), "ApplyKnockback")]
+    public class KnockbackPatch
+    {
+        public static bool Prefix(Vector3 direction, float speed)
+        {
+            return !NetworkSystem.Instance.GameModeString.Contains("MODDED_");
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using Photon.Pun;
+using System;
+using UnityEngine;
+
+namespace AntiSnowballs
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin Instance { get; private set; }
+
+        public void OnEnable() => HarmonyPatches.ApplyHarmonyPatches();
+        public void OnDisable() => HarmonyPatches.RemoveHarmonyPatches();
+    }
+    public class PluginInfo
+    {
+        internal const string
+            GUID = "John.antisnowballs",
+            Name = "AntiSnowballs",
+            Version = "1.0.0";
+    }
+}
